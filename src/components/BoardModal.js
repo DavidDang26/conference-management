9b7b3dcb47ed7285318f019aabae0556f0fef2e0
daviddang1216 <datdavid26@gmail.com>
2023-06-23 17:08:08 +0700

    init feature

--- src/components/BoardModal.js
+++ src/components/BoardModal.js
@@ -1,10 +1,17 @@
-import { Button, Input, Modal } from 'antd';
+import { Button, Input, Modal, DatePicker } from 'antd';
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
+import { ConferenceFormType } from '../Constants';
 
 export const BoardModal = (props) => {
-    const { closeModal, action, visible } = props;
-    const [boardTitle, setBoardTitle] = useState('');
+    const { closeModal, addBoard, visible, board, type } = props;
+    const defaultConference = {
+        title: '',
+        timeOccur: '',
+    };
+    const [conference, setConference] = useState(board || defaultConference);
+    const { title, timeOccur } = conference;
     const [loading, setLoading] = useState(false);
 
     const isEmptyText = (text) => !text || !text.trim();
@@ -12,36 +19,63 @@ export const BoardModal = (props) => {
     const handleCreateBoard = async (event) => {
         setLoading(true);
         event.preventDefault();
-        if (isEmptyText(boardTitle)) {
+        if (isEmptyText(title)) {
             return;
         }
-        await action({
-            title: boardTitle,
-        });
-        setBoardTitle('');
+        await addBoard(conference);
+        // setBoardTitle('');
         setLoading(false);
+    };
+
+    const handleTitleChange = (e) => {
+        setConference((conference) => ({
+            ...conference,
+            title: e.target.value,
+        }));
+    };
+
+    const handleDateChange = (date, dateString) => {
+        setConference((conference) => ({
+            ...conference,
+            timeOccur: date.toISOString(),
+        }));
     };
 
     return (
         <Modal
-            title="Add board"
+            title="Add new conference"
             width="400px"
             visible={visible}
             onCancel={closeModal}
             footer={null}
         >
             <form className={`w-full`} onSubmit={(event) => handleCreateBoard(event)}>
-                <Input
-                    className={`mb-3`}
-                    placeholder="Title"
-                    onChange={(event) => setBoardTitle(event.target.value)}
-                    value={boardTitle}
-                />
+                <div className="mb-5">
+                    <label htmlFor="title">Title *</label>
+                    <Input
+                        onChange={handleTitleChange}
+                        value={title}
+                        id="title"
+                        placeholder="Title"
+                    />
+                </div>
+
+                <div className="mb-5">
+                    <label htmlFor="timeOccur">Time occur</label>
+                    <DatePicker
+                        value={timeOccur ? moment(timeOccur) : null}
+                        showTime
+                        onChange={handleDateChange}
+                        className="w-full mb-5"
+                        id="timeOccur"
+                        defaultValue={undefined}
+                    />
+                </div>
                 <Button
                     type="primary"
                     onClick={(event) => handleCreateBoard(event)}
                     loading={loading}
-                    disabled={isEmptyText(boardTitle)}
+                    disabled={isEmptyText(title)}
                 >
                     Add
                 </Button>
