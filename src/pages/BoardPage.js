22161dc4f412babf3371350b13624713524cd23a
daviddang1216 <datdavid26@gmail.com>
2023-06-23 17:08:08 +0700

    more feature

--- src/pages/BoardPage.js
+++ src/pages/BoardPage.js
@@ -4,6 +4,7 @@ import Board from 'react-trello';
 import { boardService } from '../application/services';
 import { withAuthorization } from '../auth/auth-hoc';
 import { BoardSkeleton } from '../components/BoardSkeleton';
+import CardModal from '../components/CardModal';
 
 export const BoardPage = withRouter(
     withAuthorization((authUser) => !!authUser)((props) => {
@@ -11,6 +12,9 @@ export const BoardPage = withRouter(
             lanes: [],
         });
         const [loading, setLoading] = useState(false);
+        const [visible, setVisible] = useState(false);
+        const [activeCard, setActiveCard] = useState({});
+        const [activeLane, setActiveLane] = useState({});
 
         useEffect(() => {
             (async () => {
@@ -25,6 +29,27 @@ export const BoardPage = withRouter(
             setBoard(prepareBoard(data));
         };
 
+        const addComment = async (comment) => {
+            await boardService.updateBoard(boardId(), {
+                ...board,
+                lanes: board.lanes.map((laneItem) => {
+                    if (laneItem.id !== activeLane.id) return laneItem;
+                    else
+                        return {
+                            ...laneItem,
+                            cards: laneItem.cards.map((cardItem) => {
+                                if (cardItem.id !== activeCard.id) return cardItem;
+                                else
+                                    return {
+                                        ...cardItem,
+                                        comments: (cardItem?.comments || []).concat([comment]),
+                                    };
+                            }),
+                        };
+                }),
+            });
+        };
+
         // Fill empty properties that are important for Board component
         const prepareBoard = (board) => ({
             ...board,
@@ -36,22 +61,50 @@ export const BoardPage = withRouter(
 
         const boardId = () => props.match?.params?.board;
 
-        const handleDataChange = async (data) => await boardService.updateBoard(boardId(), data);
+        const handleDataChange = async (data) => {
+            await boardService.updateBoard(boardId(), data);
+            await fetchBoard();
+        };
 
         if (loading) {
             return <BoardSkeleton count={5} />;
         }
 
+        console.log(board);
+
         return (
-            <Board
-                className={`pt-16 bg-blue-500 h-full`}
-                canAddLanes={true}
-                editable={true}
-                data={{
-                    lanes: board.lanes || [],
-                }}
-                onDataChange={handleDataChange}
-            />
+            <>
+                <div className="text-3xl pt-16  text-center bg-blue-400 text-white font-bold">
+                    {board.title}
+                </div>
+                <Board
+                    className={`pt-5 bg-blue-400 h-full`}
+                    canAddLanes={true}
+                    editable={true}
+                    draggable={true}
+                    data={{
+                        lanes: board.lanes || [],
+                    }}
+                    onDataChange={handleDataChange}
+                    onCardClick={(cardId, metadata, laneId) => {
+                        const laneSelected = board.lanes.find((laneItem) => laneItem.id === laneId);
+                        const cardSelected = (laneSelected?.cards || []).find(
+                            (cardItem) => cardItem.id === cardId
+                        );
+                        setActiveLane(laneSelected);
+                        setActiveCard(cardSelected);
+                        setVisible(true);
+                    }}
+                    hideCardDeleteIcon={true}
+                />
+                <CardModal
+                    closeModal={() => setVisible(false)}
+                    visible={visible && activeCard.id}
+                    card={activeCard}
+                    addComment={addComment}
+                    key={activeCard?.id}
+                />
+            </>
         );
     })
 );
